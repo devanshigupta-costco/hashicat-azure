298d518648861ef0bb459ec6d18e5bd82c823d97
devanshigupta <devanshigupta@costco.com>
2019-09-04 21:43:15 +0000

    Changed

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,14 +4,14 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>Yay!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  <center><h2>Yay World!</h2></center>
+  Welcome to ${PREFIX}'s app. Hello Humans. Doggo love. This is awesome! 
   <!-- END -->
   
   </div>
